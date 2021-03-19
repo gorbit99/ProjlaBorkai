a9493b4a5e9b4e42da3e6d14e0786ecdb76ea391
Benedek Boldizsér <ardangorv@gmail.com>
2021-03-19 20:50:43 +0100

    Main skeleton

--- Classes/src/game_classes/main.java
+++ Classes/src/game_classes/main.java
@@ -3,7 +3,7 @@ package game_classes;
 import java.util.Scanner;
 public class main {
     public static void printUseCases() {
-        System.out.print("The following use cases are avilable:\n" +
+        System.out.println("The following use cases are avilable:\n" +
                         "(1) Astronaut moves to asteroid\n" +
                         "(2) Astronaut moves to Teleporter\n" +
                         "(3) Astronaut drill regular\n" +
@@ -27,7 +27,9 @@ public class main {
                         "(21) Robot drills vaporizing\n" +
                         "(22) Robot waits\n" +
                         "(23) Solarstorm happens\n" +
-                        "(24) Asteroidfield moves\n");
+                        "(24) Asteroidfield moves\n" +
+                        "(0) Exit program\n" +
+                        "Please type the number of the use-case you wish to run!");
     }
 
     public static void main(String[] args) {
@@ -36,8 +38,84 @@ public class main {
         int number = sc.nextInt();
 
         while (number != 0) {
-
             printUseCases();
+
+            switch (number) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+                case 8:
+
+                    break;
+                case 9:
+
+                    break;
+                case 10:
+
+                    break;
+                case 11:
+
+                    break;
+                case 12:
+
+                    break;
+                case 13:
+
+                    break;
+                case 14:
+
+                    break;
+                case 15:
+
+                    break;
+                case 16:
+
+                    break;
+                case 17:
+
+                    break;
+                case 18:
+
+                    break;
+                case 19:
+
+                    break;
+                case 20:
+
+                    break;
+                case 21:
+
+                    break;
+                case 22:
+
+                    break;
+                case 23:
+
+                    break;
+                case 24:
+
+                    break;
+                default:
+                    System.out.println("Error!");
+            }
 
 
             number = sc.nextInt();
