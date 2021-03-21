b459a3a9af1630398672da76522129fe86a545be
András Smuk <andras.smuk@gmail.com>
2021-03-21 19:52:41 +0100

    Asteroid (még ellenőrizni kell)

--- Classes/src/game_classes/Asteroid.java
+++ Classes/src/game_classes/Asteroid.java
@@ -4,25 +4,71 @@ package game_classes;
 import java.util.Scanner;
 
 public class Asteroid extends SpaceObject {
-	private int layers;
-	private float distanceFromSun;
 	private Material core;
-	private Asteroid neighbours;
+	public Asteroid(){
+		System.out.println("Asteroid.ctor");
+	}
+
 	public void Drill() {
+		System.out.println("Asteroid.Drill");
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Is just 1 layer on me? (y/n)");
+		String answer = sc.nextLine();
+		sc.close();
+		if (answer.equals("y")){
+			if (IsCloseToSun())
+				core.HandleCloseToSun(this);
+		}
 	}
 	
 	public Material Mine() {
-		return null;
+		System.out.println("Asteroid.Mine");
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Is 1 or more layer on me? (y/n)");
+		String answer = sc.nextLine();
+		sc.close();
+		if (answer.equals("y")){
+			return null;
+		}
+		core=null;
+		return core;
 	}
 	
 	public boolean PlaceMaterial(Material material) {
-		return true;
+		System.out.println("Asteroid.PlaceMaterial");
+
+		if(material!=null){
+			core=material;
+			if(IsCloseToSun()){
+				core.HandleCloseToSun(this);
+			}
+		}
+
+		return material==null;
 	}
 	
 	public void MoveAsteroid() {
+		System.out.println("Asteroid.MoveAsteroid");
+
+		if(IsCloseToSun()){
+			core.HandleCloseToSun(this);
+		}
 	}
 	
 	public void Explode() {
+		System.out.println("Asteroid.Explode");
+
+		for (Worker w:workers) { //itt honnan van a worker????
+			w.Explode();
+		}
+
+		for (SpaceObject so:neigbours) {
+			so.RemoveNeighbour(this);
+		}
+
+		AsteroidField.GetInstance().RemoveAsteroid(this);
 	}
 
 	public boolean CanHideIn() {
@@ -38,22 +84,44 @@ public class Asteroid extends SpaceObject {
 	}
 	
 	public void AddWorker(Worker worker) {
+		System.out.println("Asteroid.AddWorker");
+		workers.add(worker);
 	}
 	
 	public void RemoveWorker(Worker worker) {
+		System.out.println("Asteroid.RemoveWorker");
+		workers.remove(worker);
 	}
 	
 	public void SetCore(Material material) {
+		System.out.println("Asteroid.SetCore");
+		core=material;
 	}
 	
 	public void RemoveNeighbour(SpaceObject spaceObject) {
+		System.out.println("Asteroid.RemoveNeighbour");
+		neigbours.remove(spaceObject);
 	}
 	
 	public void HandleSolarStorm() {
+		System.out.println("Asteroid.HandleSolarStorm");
+		if(!CanHideIn()){
+			for (Worker w: workers) {
+				w.Die();
+			}
+		}
 	}
 	
 	public boolean IsCloseToSun() {
-		return true;
+		System.out.println("Asteroid.IsCloseToSun");
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Am I close to the sun? (y/n)");
+		String answer = sc.nextLine();
+		sc.close();
+		if (answer.equals("y"))
+			return true;
+		return false;
 	}
 
 
