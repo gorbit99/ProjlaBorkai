75c169772873e4d2d43542cac5f198f01c08164a
Adam <adam.dolnay2@gmail.com>
2021-03-21 21:21:06 +0100

    SolarStorm comments

--- Classes/src/game_classes/SolarStorm.java
+++ Classes/src/game_classes/SolarStorm.java
@@ -3,14 +3,22 @@ package game_classes;
 import java.util.Scanner;
 
 /**
- * This class represents
+ * This class represents the SolarStorms in the game.
  */
 public class SolarStorm {
 
+	/**
+	 * Constructor of the class.
+	 */
 	public SolarStorm(){
 		TestLogger.EnterFunction("SolarStorm.ctor");
 		TestLogger.ExitFunction();
 	}
+
+	/**
+	 * Decreases the object's inner counter, resets it if necessary, returns whether a solarstorm should happen.
+	 * @return True if a solarstorm is happening, false otherwise.
+	 */
 	public boolean Tick() {
 		TestLogger.EnterFunction("SolarStorm.Tick");
 		Scanner sc = new Scanner(System.in);
