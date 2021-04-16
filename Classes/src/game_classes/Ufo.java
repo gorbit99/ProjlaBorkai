8859e9c13a921d1a53d88d00cef3927adf236548
nagylev <levinagy44@gmail.com>
2021-04-16 18:01:05 +0200

    ufo class added

---
+++ Classes/src/game_classes/Ufo.java
@@ -0,0 +1,29 @@
+package game_classes;
+
+public class Ufo extends Worker{
+    @Override
+    public void Explode() {
+        Die();
+    }
+
+    @Override
+    public void Step() {
+
+    }
+
+    @Override
+    public void Move() {
+
+    }
+
+    @Override
+    public Material[] GetStoredMaterials() {
+        return null;
+    }
+
+    public void Steal(){
+
+    }
+
+
+}
