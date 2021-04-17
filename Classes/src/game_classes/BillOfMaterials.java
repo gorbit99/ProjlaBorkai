b347b26164a010e5dbdd4592b4808b7b1ce2e57b
Benedek Boldizsér <ardangorv@gmail.com>
2021-04-17 21:32:56 +0200

    BillOfMaterials

--- Classes/src/game_classes/BillOfMaterials.java
+++ Classes/src/game_classes/BillOfMaterials.java
@@ -4,15 +4,34 @@ package game_classes;
  * represents the which knows the right amount of materials for the creation of an object
  */
 public class BillOfMaterials {
+
+	Material[] billOfMaterials;
+
+	public BillOfMaterials(Material[] temp) {
+		billOfMaterials = temp;
+	}
 	/**
 	 * decides whether there is enough materials to creat an object
 	 * @param materials materials to be checked if they are enough
 	 * @return true if there is enough materials false if there isn't
 	 */
 	public boolean IsEnough(Material[] materials) {
-		TestLogger.EnterFunction("BillOfMaterials.IsEnough");
-		String answer = TestLogger.AskQuestion("Is there enough material? (y/n)");
-		TestLogger.ExitFunction();
-		return answer.equals("y");
+		Material[] materials_temp = materials.clone();
+		for(int i = 0; i < billOfMaterials.length; i++) {
+			boolean inside = false;
+
+			for(int j = 0; j < materials.length; j++) {
+				if(materials_temp[j].getClass().equals(billOfMaterials[i].getClass()) && materials_temp[j] != null) {
+					inside = true;
+					materials_temp[j] = null;
+					break;
+				}
+			}
+
+			if(!inside)
+				return false;
+		}
+
+		return true;
 	}
 }
