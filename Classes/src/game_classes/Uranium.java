327d571c7852da6951f4682e920845bc3671aa88
nagylev <levinagy44@gmail.com>
2021-04-17 20:38:28 +0200

    updated to last documentation

--- Classes/src/game_classes/Uranium.java
+++ Classes/src/game_classes/Uranium.java
@@ -3,31 +3,40 @@ package game_classes;
 /**
  * This class represents the uranium. It extends from material.
  */
-public class Uranium extends Material{
-	/**
-	 * This is the constructor of the ice.
-	 */
-	public Uranium(){
-		TestLogger.EnterFunction("Uranium.ctor");
-		TestLogger.ExitFunction();
-	}
+public class Uranium extends Material {
 
-	/**
-	 * This method makes the asteroid explode.
-	 * @param asteroid The asteroid is what contains this material.
-	 */
-	@Override
-	public void HandleCloseToSun(Asteroid asteroid) {
-		TestLogger.EnterFunction("Uranium.HandleCloseToSun");
-		asteroid.Explode();
-		TestLogger.ExitFunction();
-	}
+    /**
+     * exposureCount represents how many times has the uranium been exposed
+     * to the sun
+     */
+    private int exposureCount;
 
-	/**
-	 *
-	 * @return The class name.
-	 */
-	public String toString(){
-		return "Uranium";
-	}
+    /**
+     * This is the constructor of the ice.
+     */
+    public Uranium() {
+        TestLogger.EnterFunction("Uranium.ctor");
+        TestLogger.ExitFunction();
+    }
+
+    /**
+     * This method makes the asteroid explode.
+     *
+     * @param asteroid The asteroid is what contains this material.
+     */
+    @Override
+    public void HandleCloseToSun(Asteroid asteroid) {
+        TestLogger.EnterFunction("Uranium.HandleCloseToSun");
+        exposureCount++;
+        if (exposureCount == 3)
+            asteroid.Explode();
+        TestLogger.ExitFunction();
+    }
+
+    /**
+     * @return The class name.
+     */
+    public String toString() {
+        return "Uranium";
+    }
 }
