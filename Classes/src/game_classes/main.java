a3f03dc6795a4068490a50e85e9c2f314ab753d6
Adam <adam.dolnay2@gmail.com>
2021-04-18 00:21:14 +0200

    mock proba

--- Classes/src/game_classes/main.java
+++ Classes/src/game_classes/main.java
@@ -9,6 +9,17 @@ public class main {
      * @param args Program arguments
      */
     public static void main(String[] args) {
+        Asteroid a1 = new Asteroid();
+        Astronaut s1 = new Astronaut(a1);
+        a1.SetCore(new Coal());
+        MockIO.in.addInput("1");
+        try{
+            s1.Mine();
+            System.out.println(s1.GetStoredMaterials().size());
+            s1.PlaceMaterial();
+        } catch (Exception e) {
+        }
+        System.out.println(a1.GetCore().toString());
     }
 }
 
