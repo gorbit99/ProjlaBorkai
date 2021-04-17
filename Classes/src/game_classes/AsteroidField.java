b053541921b06c02fad3e79a5a4ce5cdff2caaa7
nagylev <levinagy44@gmail.com>
2021-04-17 20:56:10 +0200

    updated to doc

--- Classes/src/game_classes/AsteroidField.java
+++ Classes/src/game_classes/AsteroidField.java
@@ -6,78 +6,74 @@ import java.util.ArrayList;
  * This class represents the asteroid field of the game. Stores and moves the asteroids.
  */
 public class AsteroidField {
-	private ArrayList<SpaceObject> spaceObjects;
-	private static AsteroidField instance;
 
-	/**
-	 * Class constructor
-	 */
-	private AsteroidField() {
-		TestLogger.EnterFunction("AsteroidField.ctor");
-		spaceObjects = new ArrayList<>();
-		TestLogger.ExitFunction();
-	}
+    /**
+     * contains all the spaceObjects in the asteroid field
+     */
+    private ArrayList<SpaceObject> spaceObjects;
+    private static AsteroidField instance;
 
-	/**
-	 * @return the instance of the class
-	 */
-	public static AsteroidField GetInstance() {
-	    TestLogger.EnterFunction("AsteroidField.GetInstance");
-		if (instance == null)
-			instance = new AsteroidField();
-		TestLogger.ExitFunction();
-		return instance;
-	}
+    /**
+     * Class constructor
+     */
+    private AsteroidField() {
+        spaceObjects = new ArrayList<>();
+    }
 
-	/**
-	 * Moves all Asteroids in the AsteroidField by calling their MoveAsteroid() method
-	 */
-	public void Move() {
-		TestLogger.EnterFunction("AsteroidField.MoveAsteroids");
-		for (SpaceObject spaceObject: spaceObjects){
-			spaceObject.Move();
-		}
-		TestLogger.ExitFunction();
-	}
+    /**
+     * @return the instance of the class
+     */
+    public static AsteroidField GetInstance() {
+        if (instance == null)
+            instance = new AsteroidField();
+        return instance;
+    }
 
-	/**
-	 * Adds a spaceObject to the AsteroidField
-	 * @param spaceObject this spaceObject will be added to the AsteroidField
-	 */
-	public void AddSpaceObject(SpaceObject spaceObject) {
-		TestLogger.EnterFunction("AsteroidField.AddAsteroid");
-		spaceObjects.add(spaceObject);
-		TestLogger.ExitFunction();
-	}
+    /**
+     * Moves all Asteroids in the AsteroidField by calling their MoveAsteroid() method
+     */
+    public void Move() {
+        for (SpaceObject spaceObject : spaceObjects) {
+            spaceObject.Move();
+        }
+    }
 
-	/**
-	 * returns the list of spaceObjects in asteroid field
-	 * @return An ArrayList of the spaceObjects in the AsteroidField
-	 */
-	public ArrayList<SpaceObject> GetObjects() {
-		TestLogger.EnterFunction("AsteroidField.GetAsteroids");
-		TestLogger.ExitFunction();
-		return spaceObjects;
-	}
+    /**
+     * Adds a spaceObject to the AsteroidField
+     *
+     * @param spaceObject this spaceObject will be added to the AsteroidField
+     */
+    public void AddSpaceObject(SpaceObject spaceObject) {
+        spaceObjects.add(spaceObject);
+    }
 
-	/**
-	 * Removes a spaceObject
-	 * @param spaceObject this space object will be removed from the AsteroidField
-	 */
-	public void RemoveSpaceObject(SpaceObject spaceObject) {
-		TestLogger.EnterFunction("AsteroidField.RemoveAsteroid");
-		spaceObjects.remove(spaceObject);
-		TestLogger.ExitFunction();
-	}
+    /**
+     * returns the list of spaceObjects in asteroid field
+     *
+     * @return An ArrayList of the spaceObjects in the AsteroidField
+     */
+    public ArrayList<SpaceObject> GetObjects() {
+        return spaceObjects;
+    }
 
-	/**
-	 * Handles a solarstorm. Calls the HandleSolarStorm method of all Asteroids stored
-	 */
-	public void HandleSolarStorm() {
-		TestLogger.EnterFunction("AsteroidField.HandleSolarStorm");
-		for (SpaceObject spaceObject : spaceObjects) {
-			spaceObject.HandleSolarStorm();
-		}
-		TestLogger.ExitFunction();
-	}
+    /**
+     * Removes a spaceObject
+     *
+     * @param spaceObject this space object will be removed from the AsteroidField
+     */
+    public void RemoveSpaceObject(SpaceObject spaceObject) {
+        spaceObjects.remove(spaceObject);
+    }
+
+    /**
+     * chooses a random asteroid than solar storm happens on that asteroid
+     * and on all its neighbours
+     */
+    public void HandleSolarStorm() {
+        SpaceObject object = this.spaceObjects.get(Game.RandomNum(this.spaceObjects.size()));
+        object.HandleSolarStorm();
+        for (SpaceObject so : object.GetNeighbours()) {
+            so.HandleSolarStorm();
+        }
+    }
 }
