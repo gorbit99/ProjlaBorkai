cbe75b71077ab38075b47f8fceb98a629d6ea9bb
nagylev <levinagy44@gmail.com>
2021-05-02 21:52:34 +0200

    proba

--- Classes/src/game_classes/Main.java
+++ Classes/src/game_classes/Main.java
@@ -1,18 +1,54 @@
 package game_classes;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     /**
      * The main function
      *
      * @param args Program arguments
      */
     public static void main(String[] args) {
-        TestHarness testHarness = new TestHarness();
+
+        Application.launch(args);
+        /*TestHarness testHarness = new TestHarness();
         try {
             testHarness.run();
         } catch (TestHarness.TestException e) {
             System.out.println(e);
-        }
+        }*/
+    }
+
+    @Override
+    public void start(Stage stage) {
+        // Create the Text
+        Text text = new Text("Hello JavaFX");
+        // Create the VBox
+        VBox root = new VBox();
+        // Add the Text to the VBox
+        root.getChildren().add(text);
+        // Set the Size of the VBox
+        root.setMinSize(350, 250);
+
+        // Create the Scene
+        Scene scene = new Scene(root);
+
+        // Set the Properties of the Stage
+        stage.setX(100);
+        stage.setY(200);
+        stage.setMinHeight(300);
+        stage.setMinWidth(400);
+
+        // Add the scene to the Stage
+        stage.setScene(scene);
+        // Set the title of the Stage
+        stage.setTitle("Your first JavaFX Example");
+        // Display the Stage
+        stage.show();
     }
 }
 
