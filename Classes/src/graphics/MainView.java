ee2f32dca1e4ba091e0aa5cf1942b196cbd24542
nagylev <levinagy44@gmail.com>
2021-05-07 23:21:40 +0200

    first

--- Classes/src/graphics/MainView.java
+++ Classes/src/graphics/MainView.java
@@ -1,11 +1,23 @@
 package graphics;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class MainView extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
     @Override
     public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.<Parent>load(getClass().getResource("Menu.fxml"));
+        Scene scene = new Scene(root, Color.ORANGE);
 
+        stage.setScene(scene);
+        stage.show();
     }
 }
