ee33b65ceabf14984c3222d05d6912c3ed180839
nagylev <levinagy44@gmail.com>
2021-05-08 22:10:20 +0200

    not resizable start button works

---
+++ Classes/src/graphics/MainController.java
@@ -0,0 +1,9 @@
+package graphics;
+
+public class MainController {
+
+    public void Start(){
+        System.out.println("starting gamee");
+    }
+
+}
--- Classes/src/graphics/MainView.java
+++ Classes/src/graphics/MainView.java
@@ -2,7 +2,6 @@ package graphics;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
@@ -12,14 +11,16 @@ public class MainView extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
         Scene scene = new Scene(root, Color.ORANGE);
-        String css=this.getClass().getResource("menuStyle.css").toExternalForm();
+        String css = this.getClass().getResource("menuStyle.css").toExternalForm();
         scene.getStylesheets().add(css);
 
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 }
