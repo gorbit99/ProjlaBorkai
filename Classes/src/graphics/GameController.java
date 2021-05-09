c80d6fb44f248ae74d99e5275087d6197e8e8482
nagylev <levinagy44@gmail.com>
2021-05-09 14:36:38 +0200

    all buttons added

--- Classes/src/graphics/GameController.java
+++ Classes/src/graphics/GameController.java
@@ -3,13 +3,27 @@ package graphics;
 import game_classes.Game;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
 public class GameController {
 
     @FXML
+    private Button placeBtn;
+    @FXML
+    private Button robotBtn;
+    @FXML
+    private Button mineBtn;
+    @FXML
+    private Button teleportBtn;
+    @FXML
+    private Button exitBtn;
+    @FXML
+    private Text playerText;
+    @FXML
     private Button drillBtn;
     @FXML
     private Button moveBtn;
+
 
     @FXML
     protected void initialize(){
