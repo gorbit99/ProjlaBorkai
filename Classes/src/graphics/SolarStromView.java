10acd1199d67600c0684d221715bb504d63d1aff
Benedek Boldizsér <ardangorv@gmail.com>
2021-05-09 20:00:09 +0200

    SolarStorm changes

--- Classes/src/graphics/SolarStromView.java
+++ Classes/src/graphics/SolarStromView.java
@@ -1,4 +1,19 @@
 package graphics;
 
+import game_classes.Material;
+import game_classes.SolarStorm;
+import javafx.scene.image.Image;
+
+import javax.swing.text.html.ImageView;
+
 public class SolarStromView {
+    protected ImageView imageView;
+
+    public void Draw(SolarStorm solarStorm) {
+
+    }
+
+    public SolarStromView(javafx.scene.image.ImageView imageView){
+        imageView.setImage(new Image(""));
+    }
 }
