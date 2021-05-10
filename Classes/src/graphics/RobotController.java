e49cd155bcbaa725e51eeea1552826fcce8394db
nagylev <levinagy44@gmail.com>
2021-05-10 12:29:41 +0200

    Changes by ardan

--- Classes/src/graphics/RobotController.java
+++ Classes/src/graphics/RobotController.java
@@ -3,6 +3,7 @@ package graphics;
 import game_classes.*;
 
 import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 
 public class RobotController extends WorkerController {
@@ -16,11 +17,22 @@ public class RobotController extends WorkerController {
 
     public RobotController(Asteroid asteroid) {
         robot = new Robot(asteroid);
-
         view = new RobotView();
         view.DrawRobot(robot);
         robot.GetChangeEvent().addPropertyChangeListener(this);
     }
+
+    /*PropertyChangeListener AstronautChange = new PropertyChangeListener() {
+        @Override
+        public void propertyChange(PropertyChangeEvent evt) {
+            System.out.println("dik");
+            view.DrawAstronaut(astronaut);
+            if (evt.getPropertyName().equals("ActiveAstronaut") && evt.getNewValue() == astronaut) {
+                view.SetButtonStatus();
+
+            }
+        }
+    };*/
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
